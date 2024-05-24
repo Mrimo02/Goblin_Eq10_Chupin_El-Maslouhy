30475e790cf5b76510f5a2e301c1ec8d4f11d99d
HP <HP@172.16.138.30>
2024-05-24 08:55:24 +0200

    Merge branch 'main' of https://github.com/Mrimo02/Goblin_Eq10_Chupin_El-Maslouhy.git into main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("+------------------------------+");
-        System.out.println("|         GOBLIN EQ10          |");
+        System.out.println("|            GOBLIN EA10           |");
 		System.out.println("+------------------------------+");
 	}
 }
-
-//test 
